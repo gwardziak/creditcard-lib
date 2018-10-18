64315b07aac0b0314c888ae100f0e1597af03ef6
Sebastian Rajca <Sebaztian60@gmail.com>
2018-10-18 10:16:11 +0000

    Revert "describe limit method"
    This reverts commit 263a0c0b62d3aa03dbbeebfac29db1498de668a4.

--- creditcard-app/src/main/java/pl/rajca/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/rajca/creditcard/CreditCard.java
@@ -4,8 +4,7 @@ class CreditCard {
     public void assignLimit(double money) {
         
     }
-    //this method will return credit limit
-    //assigned to card
+    
     public double getLimit() {
         return 2000;
     }
