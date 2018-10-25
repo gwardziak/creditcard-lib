002caf682c20168dbf7c734314a281eefb5de706
= <=>
2018-10-25 10:04:27 +0000

    add blockCard implementation

--- creditcard-app/src/main/java/pl/rajca/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/rajca/creditcard/CreditCard.java
@@ -1,11 +1,22 @@
 package pl.rajca.creditcard;
 
 class CreditCard {
+    private boolean blocked = true;
+    
     public void assignLimit(double money) {
         
     }
     
+    
+    public boolean isBlocked(){
+        return this.blocked;
+    }
+    
     public double getLimit() {
         return 2000;
+    }
+    
+    public void block(){
+        this.blocked = true;
     }
 }
