c5a167ea9a89c039eef34a3925484ad5831aa24b
= <=>
2018-10-25 10:15:20 +0000

    add test for withdrawingMoney'

--- creditcard-app/src/test/java/pl/rajca/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/rajca/creditcard/CreditCardTest.java
@@ -14,4 +14,13 @@ public class CreditCardTest {
         Assert.assertTrue(card.getLimit() == 2000);
     }
 
+    @Test
+    public void withdrawDecreaseAvaivableFunds() {
+        CreditCard card = new CreditCard();
+        
+        card.assignLimit(2000);
+        card.withdraw(2000);
+        
+        Assert.assertTrue(card.getAccountBalance() == 0);
+    }
 }
