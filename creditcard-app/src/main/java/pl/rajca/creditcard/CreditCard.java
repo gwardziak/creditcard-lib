18de03a8c403599a7fa3c7bf5033df1fbe921514
= <=>
2018-10-25 10:27:18 +0000

    add withdraw implementation

--- creditcard-app/src/main/java/pl/rajca/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/rajca/creditcard/CreditCard.java
@@ -1,11 +1,24 @@
 package pl.rajca.creditcard;
 
 class CreditCard {
+    private double balance = 0;
+    private double limit;
+    
     public void assignLimit(double money) {
-        
+        balance = money;
+        limit = money;
     }
     
     public double getLimit() {
         return 2000;
     }
+    
+    public void withdraw(double money) {
+        this.balance = balance - money;
+    }
+    
+    public double getAccountBalance() {
+        return balance;
+    }
+    
 }
